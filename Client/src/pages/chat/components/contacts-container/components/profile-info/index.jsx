3f8ea3f391e890db5de037a7d356d3ad5089a8cb
dev-code-create <ayushkankale04@gmail.com>
2025-01-10 17:29:59 +0530

    added profile Edit Functionality and Profile Info on Contacts container

---
+++ Client/src/pages/chat/components/contacts-container/components/profile-info/index.jsx
@@ -0,0 +1,66 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { getColor } from "@/lib/utils";
+import { useAppStore } from "@/Store";
+import { HOST } from "@/utils/constants";
+import React from "react";
+import { FiEdit2 } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+const ProfileInfo = () => {
+  const { userInfo } = useAppStore();
+  const navigate = useNavigate();
+  return (
+    <div className="absolute bottom-0 flex h-16 items-center justify-between px-10 w-full bg-[#2a2b33]">
+      <div className="flex gap-3 items-center justify-center">
+        <div className="w-12 h-12 relative ">
+          <Avatar className=" h-12 w-12  rounded-full overflow-hidden">
+            {userInfo.image ? (
+              <AvatarImage
+                src={`${HOST}/${userInfo.image}`}
+                alt="Profile"
+                className="object-cover w-full h-full bg-none "
+              />
+            ) : (
+              <div
+                className={`uppercase h-12 w-12  text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(
+                  userInfo.color
+                )}`}
+              >
+                {userInfo.firstName
+                  ? userInfo.firstName.split("").shift()
+                  : userInfo.email.split("").shift()}
+              </div>
+            )}
+          </Avatar>
+        </div>
+        <div>
+          {userInfo.firstName && userInfo.lastName
+            ? `${userInfo.firstName} ${userInfo.lastName}`
+            : userInfo.email}
+        </div>
+      </div>
+      <div className="flex gap-5">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <FiEdit2
+                className="text-purple-500 text-xl font-medium"
+                onClick={() => navigate("/profile")}
+              />
+            </TooltipTrigger>
+            <TooltipContent className="bg-[#1c1b1e] border-none text-white">
+              Edit Profile
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileInfo;
